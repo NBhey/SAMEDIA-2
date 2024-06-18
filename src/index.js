1af9b0e47b59fde5ffb8d2f2d2225feedb2c5f26
Oganes <og.marckaryan@yandex.ru>
2024-06-18 10:19:58 +0300

    corrected code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const inputLogin = document.getElementById("login");
 const inputPassword = document.getElementById("password");
 const btn = document.querySelector(".authorization__button");
-const authorizationForm = document.querySelector('.authorization__form')
-const authorizationWrapper = document.querySelector('.authorization-wrap')
+const authorizationForm = document.querySelector(".authorization__form");
+const authorizationWrapper = document.querySelector(".authorization-wrap");
 
 async function request(login, password) {
   const response = await fetch(
@@ -13,30 +13,28 @@ async function request(login, password) {
     document.cookie = `token=${data.token}`;
   }
   return data;
-  
 }
 
-let authorizationResult = document.querySelector('.authorization__result')
+const authorizationResult = document.querySelector(".authorization__result");
 
 btn.addEventListener("click", async (e) => {
-    e.preventDefault();
-    const result = await request(inputLogin.value, inputPassword.value);
-    if(result.status == 'ok'){
-        inputLogin.setAttribute('style', 'color:#43A470; border-color:#43A470;');
-        inputPassword.setAttribute('style', 'border-color:#43A470;');
-        authorizationResult.innerHTML = `<img src="./src/img/загрузка.gif">`;
-        setTimeout(()=>{
-            authorizationForm.remove()
-            authorizationWrapper.innerHTML = `
+  e.preventDefault();
+  const result = await request(inputLogin.value, inputPassword.value);
+  if (result.status === "ok") {
+    inputLogin.setAttribute("style", "color:#43A470; border-color:#43A470;");
+    inputPassword.setAttribute("style", "border-color:#43A470;");
+    authorizationResult.innerHTML = `<img src="./src/img/загрузка.gif">`;
+    setTimeout(() => {
+      authorizationForm.remove();
+      authorizationWrapper.innerHTML = `
                 <img src="./src/img/flower.png" alt="авторизация-лого">
                 <h2 class="authorization__title">Добро пожаловать!</h2>
-                <p>${result.user['name']}, Вы успешно авторизованы</p>
-                `
-        },3000);
-    } else {
-        inputLogin.setAttribute('style', 'color:#F65454; border-color:#F65454;');
-        inputPassword.setAttribute('style', 'color:#F65454; border-color:#F65454;');
-        authorizationResult.innerHTML = `${result.errorMessage}`;
-    }
-  });
-
+                <p class="authorization__welcome">${result.user["name"]}, Вы успешно авторизованы</p>
+                `;
+    }, 3000);
+  } else {
+    inputLogin.setAttribute("style", "color:#F65454; border-color:#F65454;");
+    inputPassword.setAttribute("style", "color:#F65454; border-color:#F65454;");
+    authorizationResult.innerHTML = `${result.errorMessage}`;
+  }
+});
