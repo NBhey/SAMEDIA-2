c4a97f1310ad0a6d84ba9ba118f3050d979a1ab6
Oganes <og.marckaryan@yandex.ru>
2024-06-21 19:44:31 +0300

    second refactoring

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import requestLogin from './js/requestLogin.js'
-import {receivedNegativeResponse} from './js/getRequestResponse.js'
+import requestLogin from "./js/requestLogin.js";
+import { receivedNegativeResponse } from "./js/getRequestResponse.js";
 
 const inputLogin = document.getElementById("login");
 const inputPassword = document.getElementById("password");
-const btn = document.querySelector(".authorization__button");
 const authorizationForm = document.querySelector(".authorization__form");
 const authorizationWrapper = document.querySelector(".authorization-wrap");
-
-
 
 const authorizationResult = document.querySelector(".authorization__result");
 
@@ -15,12 +12,25 @@ authorizationForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const result = await requestLogin(inputLogin.value, inputPassword.value);
   if (result.status !== "ok") {
-    receivedNegativeResponse(result, inputLogin,inputPassword,authorizationResult )
+    receivedNegativeResponse(
+      result,
+      inputLogin,
+      inputPassword,
+      authorizationResult
+    );
   } else {
     document.cookie = `token=${result.token}`;
-    inputLogin.setAttribute("style", "border-color:#43A470;");
-    inputPassword.setAttribute("style", "border-color:#43A470;");
     authorizationResult.innerHTML = `<img src="./src/img/загрузка.gif">`;
+    if (
+      inputLogin.classList.contains("authorization__failure") &&
+      inputPassword.classList.contains("authorization__failure")
+    ) {
+      inputLogin.classList.remove("authorization__failure");
+      inputPassword.classList.remove("authorization__failure");
+    }
+    inputLogin.classList.add("authorization__succsess");
+    inputPassword.classList.add("authorization__succsess");
+
     setTimeout(() => {
       authorizationForm.remove();
       authorizationWrapper.innerHTML = `
@@ -28,7 +38,6 @@ authorizationForm.addEventListener("submit", async (e) => {
                 <h2 class="authorization__title">Добро пожаловать!</h2>
                 <p class="authorization__welcome">${result.user["name"]}, Вы успешно авторизованы</p>
                 `;
-    }, 1000);
-    
+    }, 500);
   }
 });
